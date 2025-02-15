a9872118c276be9491c0877d2734c76bce31a2c0
YelyelyeL2023 <okarintimetravels@gmail.com>
2025-02-15 09:50:14 +0500

    First commit

---
+++ Advanced.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import httpx
+import logging
+import asyncio
+import threading
+from telegram import Bot
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
+
+# Настройки Telegram Bot
+BOT_TOKEN = "7897738368:AAHCYVdHyJCudXQNqzKEuYBM_hXB8vs0GBg"
+TELEGRAM_CHAT_ID = "-4709248843"
+OLLAMA_API_URL = "http://localhost:11434/api/generate"
+
+# Логирование
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=BOT_TOKEN)
